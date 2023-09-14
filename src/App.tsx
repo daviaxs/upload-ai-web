8e03779a2aa6bab43c7f8f1883e331608997ee9d
Davi Alves <cadwvk@gmail.com>
2023-09-14 17:25:43 -0300

    Created aside

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import { Github } from 'lucide-react'
+import { FileVideo, Github, Upload, Wand2 } from 'lucide-react'
 import { Button } from './components/ui/button'
+import { Label } from './components/ui/label'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/select'
 import { Separator } from './components/ui/separator'
+import { Slider } from './components/ui/slider'
 import { Textarea } from './components/ui/textarea'
 
 export function App() {
@@ -43,7 +52,101 @@ export function App() {
             prompt para adicionar a transcrição do seu vídeo selecionado.
           </p>
         </div>
-        <aside className="w-80"></aside>
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6 rounded-md">
+            <label
+              htmlFor="video"
+              className="border flex aspect-video cursor-pointer border-dashed rounded-lg text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-slate-900/50 transition-colors"
+            >
+              <FileVideo className="h-8 w-8" />
+              Selecione um vídeo
+            </label>
+
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+
+            <Separator />
+
+            <div className="space-y-2">
+              <Label htmlFor="transcription_prompt">
+                Prompt de transcrição
+              </Label>
+              <Textarea
+                id="transcription_prompt"
+                className="h-20 leading-relaxed resize-none"
+                placeholder="Inclua palavras-chaves mencionadas no vídeo separadas por (,)"
+              />
+            </div>
+
+            <Button
+              type="submit"
+              className="w-full p-6 flex items-center text-white gap-4"
+            >
+              Carregar vídeo <Upload className="h-4 w-4" />
+            </Button>
+          </form>
+
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt..." />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">YouTube - Título</SelectItem>
+                  <SelectItem value="description">
+                    YouTube - Descrição
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <span className="block text-sm italic text-muted-foreground">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+
+              <Slider min={0} max={1} step={0.1} />
+
+              <span className="block text-sm italic text-muted-foreground leading-relaxed">
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type="submit" className="flex gap-2 w-full text-white p-6">
+              Executar
+              <Wand2 className="h-4 w-4" />
+            </Button>
+          </form>
+        </aside>
       </main>
     </div>
   )
