da0f77811aa74377e2e94835e9f16699aa497f03
Davi Alves <cadwvk@gmail.com>
2023-09-16 14:32:20 -0300

    Created handleUploadVideo func

--- src/components/VideoInputForm.tsx
+++ src/components/VideoInputForm.tsx
@@ -1,5 +1,5 @@
 import { FileVideo, Upload } from 'lucide-react'
-import { ChangeEvent, useMemo, useState } from 'react'
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from 'react'
 import { Button } from './ui/button'
 import { Label } from './ui/label'
 import { Separator } from './ui/separator'
@@ -7,6 +7,7 @@ import { Textarea } from './ui/textarea'
 
 export function VideoInputForm() {
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget
@@ -20,6 +21,16 @@ export function VideoInputForm() {
     setVideoFile(selectedFile)
   }
 
+  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const prompt = promptInputRef.current?.value
+
+    if (!videoFile) {
+      return
+    }
+  }
+
   const previewURL = useMemo(() => {
     if (!videoFile) {
       return null
@@ -29,7 +40,7 @@ export function VideoInputForm() {
   }, [videoFile])
 
   return (
-    <form className="space-y-6 rounded-md">
+    <form onSubmit={handleUploadVideo} className="space-y-6 rounded-md">
       <label
         htmlFor="video"
         className="relative border flex aspect-video cursor-pointer border-dashed rounded-lg text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-slate-900/50 transition-colors"
@@ -38,7 +49,7 @@ export function VideoInputForm() {
           <video
             src={previewURL}
             controls={false}
-            className="pointer-events-none absolute inset-0 rounded-lg"
+            className="pointer-events-none absolute inset-0 rounded-lg aspect-video"
           />
         ) : (
           <>
@@ -61,6 +72,7 @@ export function VideoInputForm() {
       <div className="space-y-2">
         <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
         <Textarea
+          ref={promptInputRef}
           id="transcription_prompt"
           className="h-20 leading-relaxed resize-none"
           placeholder="Inclua palavras-chaves mencionadas no vídeo separadas por (,)"
